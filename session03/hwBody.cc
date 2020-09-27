03893594f85bd693b846636be08e508458be136b
Michael DiGregorio <mjm.digregorio@gmail.com>
2020-09-27 19:54:01 -0400

    body: added file read with no any type
    Signed-off-by: Michael DiGregorio <mjm.digregorio@gmail.com>

--- session03/hwBody.cc
+++ session03/hwBody.cc
@@ -8,62 +8,25 @@
 
 using namespace std;
 
-class AnyType {
-public:
-	~AnyType() = default;
-};
-
-template<typename T>
-class Any: public AnyType {
-private:
-	T val;
-public:
-	Any(T value): val(value) {}
-	friend ostream& operator <<(ostream& s, Any a) {
-		return s << a.val;
-	}
-friend class Body;
-};
-
 class Body {
 private:
 
 	string name, orbits;
 	const double mass, diam, peri, aphe, orbper, rotper, axtlt, orbincln;
 	double x, y, z;
-	map<string, AnyType*> attrib;
-
-	void printBody() {
-		for (auto elem : attrib) {
-			cout << elem.first << " " << elem.second << endl;
-		}
-	}
-
-	void initAttribs() {
-		attrib["Name"] = new Any<string>(name);
-		attrib["Orbits"] = new Any<string>(orbits);
-		attrib["Mass(kg)"] = new Any<double>(mass);
-		attrib["Diam(m)"] = new Any<double>(diam);
-		attrib["Perihelion(m)"] = new Any<double>(peri);
-		attrib["Aphelion(m)"] = new Any<double>(aphe);
-		attrib["orbPeriod(days)"] = new Any<double>(orbper);
-		attrib["rotationalPeriod(hours)"] = new Any<double>(rotper);
-		attrib["axialtilt(deg)"] = new Any<double>(axtlt);
-		attrib["orbinclin(deg)"] = new Any<double>(orbincln);
-	}
 
 public:
 	Body(string& name, string& orbits, double mass, double diam, double peri, double aphe, double orbper, 
 		double rotper, double axtlt, double orbincln, double x, double y, double z): 
 			name(name), orbits(orbits), mass(mass), diam(diam), peri(peri), aphe(aphe), 
-			orbper(orbper), rotper(rotper), axtlt(axtlt), orbincln(orbincln), x(x), y(y), z(z) {
-			initAttribs();
-	}
+			orbper(orbper), rotper(rotper), axtlt(axtlt), orbincln(orbincln), x(x), y(y), z(z) { }
 	
 
 	friend ostream& operator <<(ostream& s, Body b) {
-		// b.printBody();
-		return s << "Name: " << b.name << " Orbits: " << b.orbits;
+		return s << "Name: " << b.name << "\n\tOrbits: " << b.orbits
+		<< "\n\tMass: " << b.mass << "\n\tDiam: " << b.diam << "\n\tPerihelion: " << b.peri
+		<< "\n\tAphelion: " << b.aphe << "\n\tOrbital Period: " << b.orbper << "\n\tRotational Period: "
+		<< b.rotper << "\n\tAxial Tilt: " << b.axtlt << "\n\tOrbital Incline: " << b.orbincln << endl;
 	}
 };
 
@@ -71,18 +34,23 @@ int main() {
 	stringstream ss; 
 	string line;
 
-	string name, orbits;
-	double mass, diam, peri, aphe, orbper, rotper, axtlt, orbincln;
-
+	string name, orbits, mass, diam, peri, aphe, orbper, rotper, axtlt, orbincln;
+	string attributes[] = {name, orbits, mass, diam, peri, aphe, orbper, rotper, axtlt, orbincln};
 	vector<Body> bodies;
 	ifstream f("solarsystem.dat");
 	while (getline(f, line)) {
-		cout << line << endl;
-		ss >> line;
-		ss << name << orbits << mass << diam << peri
-			<< aphe << orbper << rotper << axtlt << orbincln;
-		
-		bodies.push_back(Body(name, orbits, mass, diam, peri, aphe, orbper, rotper, axtlt, orbincln, 1, 2, 3));
+		if (line.substr(0, 4) == "Name")
+			continue;
+		ss << line;
+		ss >> name >> orbits >> mass >> diam >> peri >> aphe >> orbper >> rotper >> axtlt >> orbincln;
+
+		for (auto s : attributes) {
+			if (s == "NaN")
+				s = "0";
+		}
+
+		bodies.push_back(Body(name, orbits, stod(mass), stod(diam), stod(peri), stod(aphe), stod(orbper), stod(rotper), stod(axtlt), stod(orbincln), 1, 2, 3));
+		ss.clear();
 	}
 
 	for (auto body : bodies) {
