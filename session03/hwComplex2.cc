14f760351f84ea6f15d0869b9ee48028c6fa9b85
Mycicle <mjm.digregorio@gmail.com>
2020-10-30 14:26:21 -0400

    new
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- session03/hwComplex2.cc
+++ session03/hwComplex2.cc
@@ -1,17 +1,36 @@
-/*
-	Make the following main work by writing a class Complex.
-	In this second version you will overload operator << and use the friend syntax
-	
-	Replace this comment with your author information and cite as necessary.
- */
 #include <iostream>
 
+using namespace std;
+class Complex {
+private:
+	double real, im;
+public:
+	Complex(double real, double im) : real(real), im(im) {}
+
+	friend Complex operator +(Complex a, Complex b) {
+		return Complex(a.real + b.real, a.im + b.im);
+	}
+
+	friend Complex operator -(Complex c) {
+		return Complex(-c.real, c.im);
+	}
+
+	friend ostream& operator <<(ostream& s, Complex c) {
+		return s << "(" << c.real << "," << c.im << ")";
+	}
+
+	friend Complex operator *(const Complex& left, const Complex& right) {
+		return Complex(left.real*right.real, left.im*right.im);
+	}
+
+};
 
 
 int main() {
 	const Complex a(1.0, 2.0); // create a complex number with real=1.0 imag=2.0
 	const Complex b(0.5, 0.0); // real = 0.5, imag = 0.0
-	const Complex c = a + b;   // overload operator + using friend
+	const Complex c = a + b;   // overload operator + using member
 	Complex d = -c; // use friend
-	cout << c << '\t' << d << '\n';
+	const Complex e = a * b;   // complex multiplication
+	cout << c << '\t' << d << '\t' << e << '\n';
 }
